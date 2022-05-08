d7a93a15a4de0f95da9efe53394427b1445c5b4b
kmilewczyk <kmilewczyk96@gmail.com>
2022-05-08 12:28:38 +0200

    Used ngrx/component for side-nav subs

--- src/app/core/side-nav-container/material-side-nav/material-side-nav.component.ts
+++ src/app/core/side-nav-container/material-side-nav/material-side-nav.component.ts
@@ -1,22 +1,20 @@
-import { Component, Input } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
+import { Component, Input } from '@angular/core'
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout'
+import { Observable } from 'rxjs'
+import { map, shareReplay } from 'rxjs/operators'
 
 @Component({
   selector: 'app-material-side-nav',
   templateUrl: './material-side-nav.component.html',
-  styleUrls: ['./material-side-nav.component.scss']
+  styleUrls: ['./material-side-nav.component.scss'],
 })
 export class MaterialSideNavComponent {
-  @Input() sideNavTitle?: string;
+  @Input() sideNavTitle?: string
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches),
-      shareReplay()
-    );
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+    map((result) => result.matches),
+    shareReplay()
+  )
 
   constructor(private breakpointObserver: BreakpointObserver) {}
-
 }
--- src/app/core/side-nav-container/side-nav-container.module.ts
+++ src/app/core/side-nav-container/side-nav-container.module.ts
@@ -10,6 +10,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { SideNavComponent } from './side-nav/side-nav.component';
 import { TopToolbarComponent } from './top-toolbar/top-toolbar.component';
+import { ReactiveComponentModule } from '@ngrx/component';
 
 @NgModule({
   declarations: [
@@ -26,6 +27,7 @@ import { TopToolbarComponent } from './top-toolbar/top-toolbar.component';
     MatSidenavModule,
     MatIconModule,
     MatListModule,
+    ReactiveComponentModule
   ],
   exports: [
     SideNavContainerComponent
