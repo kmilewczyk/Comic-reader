fc0e3fcb0edfbc959dfd7ff19dae65e4141bda75
kmilewczyk <kmilewczyk96@gmail.com>
2022-05-11 20:55:41 +0200

    Fixed routing to work as intented.

--- src/app/core/app-container/app-container.component.ts
+++ src/app/core/app-container/app-container.component.ts
@@ -1,18 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { ConfigApiActions } from '@app/store/config/config.actions';
-import { selectConfig, StoreConfigSlice } from '@app/store/config/config.selectors';
-import { Store } from '@ngrx/store';
+import { Component } from '@angular/core';
 import { Theme } from '../_enums';
 
 @Component({
   selector: 'app-container',
   templateUrl: './app-container.component.html',
 })
-export class AppContainerComponent implements OnInit {
+export class AppContainerComponent {
   Theme = Theme;
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 }
--- src/app/routes.prod.ts
+++ src/app/routes.prod.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { AppContainerComponent } from './core/app-container/app-container.component';
+import { AppContainerModule } from './core/app-container/app-container.module';
 
 export const routes: Routes = [
   {
     path: '',
-    component: AppContainerComponent,
+    loadChildren: () => AppContainerModule
   },
 ]
