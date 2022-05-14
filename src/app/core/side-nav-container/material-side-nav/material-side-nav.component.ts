6cbe688dff0adecb29e4d8b550b66cf6ac888858
kmilewczyk <kmilewczyk96@gmail.com>
2022-05-14 19:06:56 +0200

    Moved animations to core directory

--- src/app/core/side-nav-container/material-side-nav/material-side-nav.component.ts
+++ src/app/core/side-nav-container/material-side-nav/material-side-nav.component.ts
@@ -1,18 +1,26 @@
 import { Component, Input } from '@angular/core'
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout'
-import { Observable } from 'rxjs'
-import { map, shareReplay } from 'rxjs/operators'
+import { EMPTY, Observable, of } from 'rxjs'
+import { map, shareReplay, switchMap } from 'rxjs/operators'
 import { DeviceService } from '@app/core/_services/device/device.service'
+import { heightCollapsed, HeightCollapsed } from '@app/core/_animations/height-collapsed.animation'
 
 @Component({
   selector: 'app-material-side-nav',
   templateUrl: './material-side-nav.component.html',
   styleUrls: ['./material-side-nav.component.scss'],
+  animations: [heightCollapsed],
 })
 export class MaterialSideNavComponent {
   @Input() sideNavTitle?: string
 
-  isHandset$ = this.device.isHandset$;
+  isHandset$ = this.device.isHandset$
+
+  topToolbarCollapsed$ = this.isHandset$.pipe(
+    switchMap((isHandset) =>
+      isHandset ? of(HeightCollapsed.Collapsed) : of(HeightCollapsed.Expanded)
+    )
+  )
 
   constructor(private device: DeviceService) {}
 }
